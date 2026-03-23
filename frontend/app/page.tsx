993c747296f9fcf5a4dc1e66fe63a8ce79dd2cb5
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2026-03-23 18:40:12 +0100

    refactor

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import SpotifyDownloaderApp from "@/components/spotify-downloader-app"
+
+export default function Home() {
+  return (
+    <main>
+      <SpotifyDownloaderApp />
+    </main>
+  )
+}
