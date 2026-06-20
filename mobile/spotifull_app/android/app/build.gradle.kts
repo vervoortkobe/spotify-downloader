7e4f8fcc6c2fc54221df7caa025bc1b66daa8cc9
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2026-06-21 01:11:43 +0200

    fix login

--- mobile/spotifull_app/android/app/build.gradle.kts
+++ mobile/spotifull_app/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 android {
-    namespace = "com.scooby.spotifull"
+    namespace = "com.example.spotifull"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -19,7 +19,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.scooby.spotifull"
+        applicationId = "com.example.spotifull"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
